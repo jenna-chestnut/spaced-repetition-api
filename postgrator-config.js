3fd63f9c97ddd21ab38fc9e3a0898eecc1591aaa
Jenna Chestnut <jennabot5000@gmail.com>
2021-01-25 12:33:13 -0600

    initial commit for capstone

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  "migrationDirectory": "migrations",
-  "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
-}
+  'migrationDirectory': 'migrations',
+  'driver': 'pg',
+  'host': process.env.MIGRATION_DB_HOST,
+  'port': process.env.MIGRATION_DB_PORT,
+  'database': (process.env.NODE_ENV === 'test')
+    ? process.env.MIGRATION_TEST_DB_NAME : process.env.MIGRATION_DB_NAME,
+  'username': process.env.MIGRATION_DB_USER,
+  'password': process.env.MIGRATION_DB_PASS
+};
--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-TRUNCATE
+TRUNCATE 
   "word",
   "language",
   "user";
@@ -21,14 +21,17 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'fleur', 'flower', 2),
+  (2, 1, 'ours', 'bear', 3),
+  (3, 1, 'ordinateur', 'computer', 4),
+  (4, 1, 'pomme', 'apple', 5),
+  (5, 1, 'errer', 'wander', 6),
+  (6, 1, 'la peinture', 'painting', 7),
+  (7, 1, 'lumière', 'light', 8),
+  (8, 1, 'acide', 'sour', 9),
+  (9, 1, 'scintillait', 'sparkle', 10),
+  (10, 1, 'abeille', 'bee', 11),
+  (11, 1, 'ciel', 'sky', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
